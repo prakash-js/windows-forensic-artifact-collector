204fbfe391bc7e74bf16f7e5c643a5908f576c6b
Saiprakash J <jsprakash632@gmail.com>
2026-03-05 22:33:45 +0530

    Update config_file.py

--- dbs/config_file.py
+++ dbs/config_file.py
@@ -1,7 +1,7 @@
 class configuration_file:
     def __init__(self):
         self.api_key = {
-            "api_value": "65vq2h7i7182d347fme6ad6sb63ddec6p3nkddm086a81a3acbr4bf0ad9ed81e5"
+            "api_value": "your_hybrid_analysis_api_key"
         }
 
         self.whitelisted_ips = {
@@ -10,12 +10,13 @@ class configuration_file:
                            '8.8.4.4' ,
                            '1.1.1.1',
                            '224.0.0.251',
-                           'ff02::fb','::1',
-                           '142.251.32.110'}
+                           'ff02::fb','::1'
+                           }
         }
 
         self.directory_config = {
-            "directories": ['C:\AMD\Radeon-Software-Adrenalin-2020-22.6.1-Win10-Win11-64Bit-LegacyASICs-June23-2022-LEGACY\Bin64',
-                            'C:\Program Files\Java'
+            "directories": ['',
+                
                             ]
+
         }
