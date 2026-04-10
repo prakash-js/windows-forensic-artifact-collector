f980d80e900b110ee80073b8f292eac39a6ffa44
Saiprakash J <jsprakash632@gmail.com>
2026-04-11 02:47:13 +0530

    Update live_connection_report_template.py

--- html_templates/live_connection_report_template.py
+++ html_templates/live_connection_report_template.py
@@ -60,7 +60,16 @@ class network_op:
 
         # WD missing
         if not os.path.exists(self.TCP):
-            self.html += """
+            self.html += f"""
+               <h1>{db.TCP["Heading"]}</h2>
+                <h2>{db.TCP["Title"]}</h2>
+                <p>
+                    {db.TCP["first_para"]}
+                </p>
+
+                    <p>
+                    {db.TCP["second_para"]}
+                </p>
             <h2 style="color:#555; font-family: Arial, sans-serif; margin-top:20px;">
                 No Related Artifacts Were Identified on This System
             </h2>
@@ -119,6 +128,7 @@ class network_op:
         if not os.path.exists(self.UDP):
             self.html += f"""
             <br>
+            <hr>
             <h2>{db.UDP["Title"]}</h2>
 
             <p>
